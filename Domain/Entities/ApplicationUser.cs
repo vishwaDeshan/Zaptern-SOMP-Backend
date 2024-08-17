20ff6844ed6913d539e2b4009987ab8f8ba46232
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-08-18 01:14:26 +0530

    Add project files.

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+	public class ApplicationUser
+	{
+        public Guid Id { get; set; }
+		public string? Name { get; set; }
+		public string? Email { get; set; }
+		public string? Password { get; set; }
+
+    }
+}
---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjection
+{
+	public static class ServiceContainer
+	{
+		public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<AppDbContext>(options =>
+				options.UseSqlServer(
+					configuration.GetConnectionString("Default"),
+					b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+				), ServiceLifetime.Scoped);
+
+			return services;
+		}
+	}
+}
