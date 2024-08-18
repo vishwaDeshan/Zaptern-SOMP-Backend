752c0e7093a6576867f0eeccdef374a30fc552c6
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-08-18 21:22:37 +0530

    minor changes

---
+++ Application/Common/Interfaces/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using Application.UserAccount.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IAccount
+    {
+        Task<RegistrationResponse> RegisterAccountAsync(RegisterUserDTO model);
+        Task<LoginResponse> LogInAccountAsync(LoginUserDTO model);
+    }
+}
--- Application/Services/Authentication/AccountService.cs
+++ Application/Services/Authentication/AccountService.cs
@@ -1,10 +1,7 @@
-﻿using Application.UserAccount.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Application.Common.Interfaces;
+using Application.UserAccount.DTOs;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Application.Services.Authentication
 {
