0652cb5e005c0412b6c7dc6f990f501ceb0c03b5
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-02 08:36:32 +0530

    reteive applicant list function

---
+++ API/Controllers/ApplicantController.cs
@@ -0,0 +1,23 @@
+﻿using API.Controllers;
+using Application.Applicants;
+using Application.Applicants.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[Route("api/applicant")]
+	[ApiController]
+	[AllowAnonymous] // temporarily set anonymous
+	public class ApplicantController : BaseApiController
+	{
+
+		[HttpGet("getApplicants")]
+		public async Task<List<ApplicantDto>> Get([FromQuery] GetAllApplicantRequestQuery query)
+		{
+			return await this.Mediator.Send(query);
+		}
+	}
+}
+
+
---
+++ Application/Applicants/Queries/GetAllApplicantRequestQuery.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Applicants.Queries
+{
+	public class GetAllApplicantRequestQuery : IRequest<List<ApplicantDto>> { }
+
+	public class GetAllApplicantRequestHandler : IRequestHandler<GetAllApplicantRequestQuery, List<ApplicantDto>>
+	{
+		private readonly IApplicationDbContext _applicationDbContext;
+
+		public GetAllApplicantRequestHandler(IApplicationDbContext applicationDbContext)
+		{
+			_applicationDbContext = applicationDbContext;
+		}
+
+		public async Task<List<ApplicantDto>> Handle(GetAllApplicantRequestQuery request, CancellationToken cancellationToken)
+		{
+			try
+			{
+				var applicants = await _applicationDbContext.Applicants.Select(a => new ApplicantDto
+				{
+					ApplicantId = a.ApplicantId,
+					PhoneNumber = a.PhoneNumber,
+					HomeAddress = a.HomeAddress,
+					HomeCity = a.HomeCity,
+					LandLine = a.LandLine,
+					NationalId = a.NationalId
+				}).ToListAsync(cancellationToken: cancellationToken);
+
+				if (!applicants.Any())
+				{
+					throw new NotFoundException("Applicant");
+				}
+
+				return applicants;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+	}
+}
