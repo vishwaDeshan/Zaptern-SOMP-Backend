e5784f5a6a0ec3b2b238765e1289ed119089360f
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-23 07:02:30 +0530

    register all auto mapper mappings

--- Infrastructure/DependencyInjection/ServiceContainer.cs
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
 using System.Text;
 
 namespace Infrastructure.DependencyInjection
@@ -50,7 +51,9 @@ namespace Infrastructure.DependencyInjection
 			});
 			services.AddScoped<IUser, UserRepository>();
 			services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-			services.AddAutoMapper(typeof(ApplicantMappingProfile));
+
+			//all automapper mappings
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
 			services.AddScoped<IApplicationDbContext>(provider => (IApplicationDbContext)provider.GetService<AppDbContext>());
 			services.AddHttpContextAccessor();
 
