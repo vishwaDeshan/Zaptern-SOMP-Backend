0bb474ddd7787b8d612dbc8c84cc20a7321d5e93
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-23 08:12:19 +0530

    get all eductional details

--- Application/EducationDetails/Queries/GetAllEducatioanlDetailsRequestQuery.cs
+++ Application/EducationDetails/Queries/GetAllEducatioanlDetailsRequestQuery.cs
@@ -1,11 +1,13 @@
 ﻿using Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.EducationDetails.Queries
 {
 	public class GetAllEducationalDetailsRequestQuery : IRequest<List<EducationalDetailsDto>>{ }
 
-	public class GetAllEducationalRequestQueryHandler : IRequestHandler<GetAllEducationalDetailsRequestQuery, List<EducationalDetailsDto>>{
+	public class GetAllEducationalRequestQueryHandler : IRequestHandler<GetAllEducationalDetailsRequestQuery, List<EducationalDetailsDto>>
+	{
 
 		private readonly IApplicationDbContext _applicationDbContext;
 
@@ -13,9 +15,29 @@ namespace Application.EducationDetails.Queries
 		{
 			_applicationDbContext = applicationDbContext;
 		}
-		public Task<List<EducationalDetailsDto>> Handle(GetAllEducationalDetailsRequestQuery request, CancellationToken cancellationToken)
+		public async Task<List<EducationalDetailsDto>> Handle(GetAllEducationalDetailsRequestQuery request, CancellationToken cancellationToken)
 		{
-			return null;
+			try
+			{
+				ArgumentNullException.ThrowIfNull(request);
+
+				var result = await _applicationDbContext.EducationalDetails.Select(a => new EducationalDetailsDto
+				{
+					ApplicantId = a.Applicant.ApplicantId,
+					Description = a.Description,
+					EndDate = a.EndDate,
+					InstituteName = a.InstituteName,
+					StartDate = a.StartDate,
+					IsDoing = a.IsDoing
+
+				}).ToListAsync(cancellationToken);
+
+				return result;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
 		}
 	}
 }
