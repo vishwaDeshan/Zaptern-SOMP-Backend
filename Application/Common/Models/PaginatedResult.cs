6dced18824d98fd031ff7c4ae5b66205eb9c3b4d
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-03-23 11:52:29 +0530

    search aplicants

---
+++ Application/Common/Models/PaginatedResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Models
+{
+	public class PaginatedResult<T>
+	{
+		public List<T> Items { get; set; }
+		public int TotalCount { get; set; }
+		public int PageNumber { get; set; }
+		public int PageSize { get; set; }
+
+		public PaginatedResult(List<T> items, int totalCount, int pageNumber, int pageSize)
+		{
+			Items = items;
+			TotalCount = totalCount;
+			PageNumber = pageNumber;
+			PageSize = pageSize;
+		}
+	}
+
+}
